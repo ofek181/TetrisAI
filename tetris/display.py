ac9a4edbe342dfc59a647129591998615b27831c
Ofek Ophir <ophir.ofek@gmail.com>
2022-02-03 20:20:01 +0200

    added game over screen

--- tetris/display.py
+++ tetris/display.py
@@ -88,6 +88,24 @@ class Display:
         self.window.blit(txt, (top_left_x, top_left_y))
         self.window.blit(scr, (top_left_x + 100, top_left_y))
 
+    def draw_game_over(self, score: int, highest_score: int) -> None:
+        font = pygame.font.SysFont(DisplayConsts.FONT, DisplayConsts.FONT_SIZE)
+        txt1 = font.render('Game over', True, DisplayConsts.FONT_COLOR)
+        scr = font.render('Score: ' + str(score), True, DisplayConsts.FONT_COLOR)
+        highest_scr = font.render('Highest Score: ' + str(highest_score), True, DisplayConsts.FONT_COLOR)
+        txt2 = font.render('Press any key to continue', True, DisplayConsts.FONT_COLOR)
+        display_x = DisplayConsts.TOP_LEFT_X + DisplayConsts.PLAY_WIDTH // 2 - DisplayConsts.FONT_SIZE // 2
+        display_y = DisplayConsts.TOP_LEFT_Y + DisplayConsts.PLAY_HEIGHT // 2 - DisplayConsts.FONT_SIZE * 5
+        buff = 60
+
+        self.window.fill(DisplayConsts.BLACK)
+        self.window.blit(txt1, (display_x, display_y))
+        self.window.blit(txt2, (display_x, display_y + int(buff * 0.8)))
+        self.window.blit(scr, (display_x, display_y + buff * 3))
+        self.window.blit(highest_scr, (display_x, display_y + int(buff * 3.8)))
+
+        pygame.display.update()
+
     def draw_screen(self, grid: list, positions: list, color: tuple, score: int) -> None:
         """
             Implements a frame of the game.
