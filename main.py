d6505f7670bc17a882abff02c774e36a118bf2e9
Ofek Ophir <ophir.ofek@gmail.com>
2022-02-04 12:37:14 +0200

    implemented a main function to play the game

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-import pygame
-import pygame_menu
-from consts import DisplayConsts
+from tetris.game import Game
 
 
 def main():
-    pygame.init()
-    surface = pygame.display.set_mode((DisplayConsts.SCREEN_WIDTH, DisplayConsts.SCREEN_HEIGHT))
-    # tetris = Tetris()
-    menu = pygame_menu.Menu('Tetris', DisplayConsts.SCREEN_WIDTH, DisplayConsts.SCREEN_HEIGHT,
-                            theme=pygame_menu.themes.THEME_DARK)
-    # menu.add.button('Play', AI.test)
-    # menu.add.button('Test', AI.train)
-    # menu.add.button('Train', tetris.run_game)
-    menu.add.button('Exit', pygame_menu.events.EXIT)
-    menu.mainloop(surface)
+    play = True  # play or train a neural network
+    is_player_human = True  # a human is playing or an algorithm
+    set_audio = True  # set audio on/off
+
+    if play:
+        game = Game()
+        game.play(is_player_human, set_audio)
+    else:
+        pass
 
 
 if __name__ == '__main__':
