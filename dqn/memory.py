c5d377a52ea26bdc902c859ce3f63520fb00b733
Ofek Ophir <ophir.ofek@gmail.com>
2022-02-06 20:28:43 +0200

    added docstrings

--- dqn/memory.py
+++ dqn/memory.py
@@ -7,16 +7,31 @@ Transition = namedtuple('Transition',
 
 
 class ReplayMemory:
-
+    """
+        Implementing experience replay memory for the DQN training.
+        The memory stores the observations that the agent experiences. (s,a,s',r)
+        By sampling from it randomly, we decorrelate the transitions.
+    """
     def __init__(self, capacity):
+        """
+            Initiates the memory.
+        """
         self.memory = deque([], maxlen=capacity)
 
     def push(self, *args):
-        """Save a transition"""
+        """
+            Saves a transition (namedtuple).
+        """
         self.memory.append(Transition(*args))
 
     def sample(self, batch_size):
+        """
+            Randomly samples from the memory.
+        """
         return random.sample(self.memory, batch_size)
 
     def __len__(self):
+        """
+            Returns the length of the memory.
+        """
         return len(self.memory)
