059f7c4c09e39b025b791e2480f680c8a5a9e88d
Ofek Ophir <ophir.ofek@gmail.com>
2022-02-06 22:50:34 +0200

    TODO: use tensorflow instead of pytorch

---
+++ dqn/train.py
@@ -0,0 +1,252 @@
+import os
+import shutil
+import random
+from itertools import count
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+# import torchvision.transforms as T
+import math
+
+from model import DeepQNetwork
+from memory import ReplayMemory
+from tetris.api import Environment
+from collections import namedtuple, deque
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+BATCH_SIZE = 128
+GAMMA = 0.999
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 1000
+TARGET_UPDATE = 10
+
+n_actions = 3
+policy_net = DeepQNetwork().to(device)
+target_net = DeepQNetwork().to(device)
+target_net.load_state_dict(policy_net.state_dict())
+target_net.eval()
+
+optimizer = optim.RMSprop(policy_net.parameters())
+memory = ReplayMemory(10000)
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+env = Environment()
+steps_done = 0
+
+# TODO throw away everything and use tensorflow
+
+def optimize_model():
+    if len(memory) < BATCH_SIZE:
+        return
+    transitions = memory.sample(BATCH_SIZE)
+    # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
+    # detailed explanation). This converts batch-array of Transitions
+    # to Transition of batch-arrays.
+    batch = Transition(*zip(*transitions))
+
+    # Compute a mask of non-final states and concatenate the batch elements
+    # (a final state would've been the one after which simulation ended)
+    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                          batch.next_state)), device=device, dtype=torch.bool)
+    non_final_next_states = torch.cat([s for s in batch.next_state
+                                                if s is not None])
+    state_batch = torch.cat(batch.state)
+    action_batch = torch.cat(batch.action)
+    reward_batch = torch.cat(batch.reward)
+
+    # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+    # columns of actions taken. These are the actions which would've been taken
+    # for each batch state according to policy_net
+    state_action_values = policy_net(state_batch).gather(1, action_batch)
+
+    # Compute V(s_{t+1}) for all next states.
+    # Expected values of actions for non_final_next_states are computed based
+    # on the "older" target_net; selecting their best reward with max(1)[0].
+    # This is merged based on the mask, such that we'll have either the expected
+    # state value or 0 in case the state was final.
+    next_state_values = torch.zeros(BATCH_SIZE, device=device)
+    next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0].detach()
+    # Compute the expected Q values
+    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+
+    # Compute Huber loss
+    criterion = nn.SmoothL1Loss()
+    loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+
+    # Optimize the model
+    optimizer.zero_grad()
+    loss.backward()
+    for param in policy_net.parameters():
+        param.grad.data.clamp_(-1, 1)
+    optimizer.step()
+
+
+def select_action(state):
+    global steps_done
+    random_action = random.randint(0, 2)
+    random_action_prob = random.randrange(0, 1)
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * math.exp(-1. * steps_done / EPS_DECAY)
+    steps_done += 1
+    if random_action_prob > eps_threshold:
+        with torch.no_grad():
+            return policy_net(state)
+    else:
+        return random_action
+
+
+def train():
+    num_episodes = 100
+    for i_episode in range(num_episodes):
+        # Initialize the environment and state
+        env.reset()
+        state = env.get_state()
+        while not env.game_over:
+            # Select and perform an action
+            env.play()
+            action = select_action(state)
+
+            # _, reward, done, _ = env.step(action)
+            # reward = torch.tensor([reward], device=device)
+
+            # Observe new state
+            last_screen = current_screen
+            current_screen = get_screen()
+            if not done:
+                next_state = current_screen - last_screen
+            else:
+                next_state = None
+
+            # Store the transition in memory
+            memory.push(state, action, next_state, reward)
+
+            # Move to the next state
+            state = next_state
+
+            # Perform one step of the optimization (on the policy network)
+            optimize_model()
+            if done:
+                episode_durations.append(t + 1)
+                plot_durations()
+                break
+        # Update the target network, copying all weights and biases in DQN
+        if i_episode % TARGET_UPDATE == 0:
+            target_net.load_state_dict(policy_net.state_dict())
+
+    print('Complete')
+    env.render()
+    env.close()
+    plt.ioff()
+    plt.show()
+
+
+def train():
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(0)
+    else:
+        torch.manual_seed(0)
+
+    env = Environment()
+    model = DeepQNetwork()
+    optimizer = torch.optim.Adam(model.parameters(), lr=opt.lr)
+    criterion = nn.MSELoss()
+
+    if torch.cuda.is_available():
+        model.cuda()
+
+    replay_memory = deque()
+    epoch = 0
+    while epoch < opt.num_epochs:
+        next_steps = env.get_next_states()
+        # Exploration or exploitation
+        epsilon = opt.final_epsilon + (max(opt.num_decay_epochs - epoch, 0) * (
+                opt.initial_epsilon - opt.final_epsilon) / opt.num_decay_epochs)
+        u = random()
+        random_action = u <= epsilon
+        next_actions, next_states = zip(*next_steps.items())
+        next_states = torch.stack(next_states)
+        if torch.cuda.is_available():
+            next_states = next_states.cuda()
+        model.eval()
+        with torch.no_grad():
+            predictions = model(next_states)[:, 0]
+        model.train()
+        if random_action:
+            index = randint(0, len(next_steps) - 1)
+        else:
+            index = torch.argmax(predictions).item()
+
+        next_state = next_states[index, :]
+        action = next_actions[index]
+
+        reward, done = env.step(action, render=True)
+
+        if torch.cuda.is_available():
+            next_state = next_state.cuda()
+        replay_memory.append([state, reward, next_state, done])
+        if done:
+            final_score = env.score
+            final_tetrominoes = env.tetrominoes
+            final_cleared_lines = env.cleared_lines
+            state = env.reset()
+            if torch.cuda.is_available():
+                state = state.cuda()
+        else:
+            state = next_state
+            continue
+        if len(replay_memory) < opt.replay_memory_size / 10:
+            continue
+        epoch += 1
+        batch = sample(replay_memory, min(len(replay_memory), opt.batch_size))
+        state_batch, reward_batch, next_state_batch, done_batch = zip(*batch)
+        state_batch = torch.stack(tuple(state for state in state_batch))
+        reward_batch = torch.from_numpy(np.array(reward_batch, dtype=np.float32)[:, None])
+        next_state_batch = torch.stack(tuple(state for state in next_state_batch))
+
+        if torch.cuda.is_available():
+            state_batch = state_batch.cuda()
+            reward_batch = reward_batch.cuda()
+            next_state_batch = next_state_batch.cuda()
+
+        q_values = model(state_batch)
+        model.eval()
+        with torch.no_grad():
+            next_prediction_batch = model(next_state_batch)
+        model.train()
+
+        y_batch = torch.cat(
+            tuple(reward if done else reward + opt.gamma * prediction for reward, done, prediction in
+                  zip(reward_batch, done_batch, next_prediction_batch)))[:, None]
+
+        optimizer.zero_grad()
+        loss = criterion(q_values, y_batch)
+        loss.backward()
+        optimizer.step()
+
+        print("Epoch: {}/{}, Action: {}, Score: {}, Tetrominoes {}, Cleared lines: {}".format(
+            epoch,
+            opt.num_epochs,
+            action,
+            final_score,
+            final_tetrominoes,
+            final_cleared_lines))
+        writer.add_scalar('Train/Score', final_score, epoch - 1)
+        writer.add_scalar('Train/Tetrominoes', final_tetrominoes, epoch - 1)
+        writer.add_scalar('Train/Cleared lines', final_cleared_lines, epoch - 1)
+
+        if epoch > 0 and epoch % opt.save_interval == 0:
+            torch.save(model, "{}/tetris_{}".format(opt.saved_path, epoch))
+
+    torch.save(model, "{}/tetris".format(opt.saved_path))
+
+
+if __name__ == "__main__":
+    opt = get_args()
+    train(opt)
