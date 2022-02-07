d90f13d996b5096848c6787e719d7345ad84d712
Ofek Ophir <ophir.ofek@gmail.com>
2022-02-07 15:27:55 +0200

    new model based on the deepmind atari games article

--- dqn/model.py
+++ dqn/model.py
@@ -4,21 +4,38 @@ from consts import GameConsts
 
 class DeepQNetwork:
 	def __init__(self):
-		input_size = GameConsts.GRID_WIDTH * GameConsts.GRID_HEIGHT + 1
+		input_shape = (None, GameConsts.GRID_WIDTH, GameConsts.GRID_HEIGHT)
 		tf.compat.v1.disable_eager_execution()
-		self.input = tf.compat.v1.placeholder(tf.float32, shape=input_size, name="input")
+		self.input = tf.compat.v1.placeholder(tf.float32, shape=input_shape, name="input")
 
-		layer1 = tf.keras.layers.Dense(units=input_size, activation='relu')(self.input)
-		layer2 = tf.keras.layers.Dense(units=128, activation='relu')(layer1)
-		layer3 = tf.keras.layers.Dense(units=10, activation='relu')(layer2)
+		conv_layer_1 = tf.keras.layers.Conv2D(
+			filters=16,
+			kernel_size=(8, 8),
+			strides=(4, 4),
+			activation='relu',
+			padding="same",
+			input_shape=input_shape)(self.input)
 
-		self.output = tf.keras.layers.Dense(units=4)(layer3)
+		conv_layer_2 = tf.keras.layers.Conv2D(
+			filters=32,
+			kernel_size=(4, 4),
+			strides=(2, 2),
+			activation='relu',
+			padding="same")(conv_layer_1)
+
+		conv_2_flat = tf.keras.layers.Flatten()(conv_layer_2)
+
+		hidden = tf.keras.layers.Dense(
+			units=256,
+			activation='relu')(conv_2_flat)
+
+		self.output_Q = tf.keras.layers.Dense(units=4)(hidden)
 
 		# Predict
-		self.predict_action = tf.argmax(self.output, 1)
+		self.predict_action = tf.argmax(self.output_Q, 1)
 
 		# Train
 		self.target_Q = tf.compat.v1.placeholder(tf.float32, [None, 4])
 		self.learning_rate = tf.compat.v1.placeholder(tf.float32, name="learning_rate")
-		self.loss = tf.losses.mean_squared_error(self.target_Q, self.output)
+		self.loss = tf.losses.mean_squared_error(self.target_Q, self.output_Q)
 		self.update_model = tf.compat.v1.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
